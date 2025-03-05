86411541a65dfedc2dbac4bd99455c675e016ad7
silizonetech <afzalmalik494@gmail.com>
2025-03-05 16:52:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('nav ul li a').click(function(event){
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 800);
-    });
-});
